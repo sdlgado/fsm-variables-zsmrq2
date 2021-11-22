12294007921957693137076003c17d3b2c7c6f72
sdlgado <sergio@dgtl.ec>
2021-11-22 14:53:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fsm-variables-zsmrq2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fsm-variables-zsmrq2)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+
+// TASK
+// Create 2 variables 👇
+// Variable: firstName with value of your firstName 
+// Variable: lastName with value of your lastName
+// Console log it out
+// EXTRA CREDIT: Concatinate firstName and lastName into variable fullName 🔥
+
+let firstName = "Sergio";
+let lastName = "Delgado";
+
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName)
